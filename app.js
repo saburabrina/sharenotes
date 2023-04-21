7e9e6344d4c5b6184e0b96d08eee062f9574378d
saburabrina <sabrinascosme@gmail.com>
2023-04-21 09:40:30 -0300

    add helmet library

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
+var helmet = require('helmet');
+const mongoose = require('mongoose'); 
 const config = require('./config');
 
 dburl = config.dburl;
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
